70eef03237e2e99454d9c94a2ac96c4a5b574528
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-07-15 19:41:32 +0500

    Scramble Text Checking

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { gsap } from "gsap";
+    
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+
+gsap.registerPlugin(ScrambleTextPlugin);
 
 const Navbar = () => {
+useEffect(() => {
+    const tl = gsap.timeline({
+      id: "text-scramble",
+      defaults: { ease: "none" },
+      repeat: -1,
+      repeatDelay: 2
+    });
+
+    const texts = [
+      "Mix it up with ScrambleText.",
+      "Animate your words easily.",
+      "GSAP makes it simple.",
+      "Customize your style!",
+      "Let's scramble again!"
+    ];
+
+    texts.forEach((line, index) => {
+      tl.to("#scramble-text-1", {
+        scrambleText: {
+          text: line,
+          chars: "$%@#!"
+        },
+        duration: 1,
+        delay: index === 0 ? 0 : 3
+      });
+    });
+  }, [])
+
     return (
-        <div>Navbar</div>
+        <div id="scramble-text-1"> </div>
     )
 }
 
