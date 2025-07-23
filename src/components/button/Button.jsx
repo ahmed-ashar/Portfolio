be515d7942bf5f43210a6e03535638173ba931e3
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-07-24 04:30:26 +0500

    magnet button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Magnet from '../../utilis/magnet/Magnet'
 
 const Button = ({text}) => {
   return (
     <div>
+        <Magnet padding={20} disabled={false} magnetStrength={5}>
       <button className='p-3 bg-amber-700'>{text}</button>
+        </Magnet>
     </div>
   )
 }
