10ef8754673c6639a0ce64057fab66a57e65ac78
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-07-28 00:05:20 +0500

    changed color

--- src/components/projectPage/ProjectPage.jsx
+++ src/components/projectPage/ProjectPage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProjectPage = () => {
 
-    
+
 
   return (
-    <div>
+    <div className='text-[#b7ab98] text-3xl font-extrabold flex flex-col items-center justify-center h-screen'>
 From minimalist UIs to fully functional apps — here’s a glimpse of what I build.      
     </div>
   )
